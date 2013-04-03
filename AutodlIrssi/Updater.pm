9abbb6abefd6a7af0522c9613830893013f40a9f
thebigmunch <munchicus@gmail.com>
2013-04-03 10:58:44 -0400

    Change update URL to defeat caching

--- AutodlIrssi/Updater.pm
+++ AutodlIrssi/Updater.pm
@@ -40,7 +40,7 @@ use File::Spec;
 use File::Copy;
 use Archive::Zip qw/ :ERROR_CODES /;
 use constant {
-	UPDATE_URL => 'https://autodl-irssi-community.googlecode.com/files/update.xml',
+	UPDATE_URL => 'https://autodl-irssi-community.googlecode.com/files/update.xml?nocache',
 
 	# This must not be a popular web browser's user agent or the update may fail
 	# since SourceForge checks the user agent and sends different results depending
